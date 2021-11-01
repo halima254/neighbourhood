d2ca41c3da402dc702967e7a7cbd92471d223b50
halima254 <halima.yahya@student.moringaschool.com>
2021-11-01 18:12:02 +0300

    add update profile form

--- hood/forms.py
+++ hood/forms.py
@@ -11,3 +11,9 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        exclude = ('user', 'neighbourhood')
+        
